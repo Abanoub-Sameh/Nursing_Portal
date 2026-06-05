d10235d7aefc04a821d28dddad3c9efaefc478c4
Abanoub Sameh <124413860+Abanoub-Sameh@users.noreply.github.com>
2026-06-06 02:19:52 +0300

    Delete lecture 4

--- curriculum.js
+++ curriculum.js
@@ -41,18 +41,6 @@ const curriculumData = {
               }
             },
             {
-              "num": 4,
-              "title_ar": "33",
-              "files": {
-                "original_ppt": [],
-                "translated_ppt": [],
-                "my_quiz": [],
-                "doctor_quiz": [
-                  "terms/term_1/anatomy/quizzes/Lecture_2_Prefixes_and_Procedures.html"
-                ]
-              }
-            },
-            {
               "num": 5,
               "title_ar": "66",
               "files": {
