15d2d53f9455cfdea744b27ef4dc63aca93159fe
Abanoub Sameh <124413860+Abanoub-Sameh@users.noreply.github.com>
2026-06-06 04:37:43 +0300

    Delete lecture 13

--- curriculum.js
+++ curriculum.js
@@ -149,18 +149,6 @@ const curriculumData = {
               }
             },
             {
-              "num": 13,
-              "title_ar": "4685",
-              "files": {
-                "original_ppt": [],
-                "translated_ppt": [],
-                "my_quiz": [
-                  "terms/term_1/anatomy/quizzes/Lecture_3_Reproductive_System.html"
-                ],
-                "doctor_quiz": []
-              }
-            },
-            {
               "num": 14,
               "title_ar": "ىىىىى",
               "files": {
