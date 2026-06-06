2c252f94b475de2b9c40964d2b9a979c45071b52
Abanoub Sameh <124413860+Abanoub-Sameh@users.noreply.github.com>
2026-06-06 04:27:08 +0300

    Add lecture 14

--- curriculum.js
+++ curriculum.js
@@ -159,6 +159,18 @@ const curriculumData = {
                 ],
                 "doctor_quiz": []
               }
+            },
+            {
+              "num": 14,
+              "title_ar": "ىىىىى",
+              "files": {
+                "original_ppt": [],
+                "translated_ppt": [],
+                "my_quiz": [
+                  "terms/term_1/anatomy/quizzes/Lecture_2_Prefixes_and_Procedures.html"
+                ],
+                "doctor_quiz": []
+              }
             }
           ]
         },
