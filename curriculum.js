3ae1670a44bc0e2e14c3437b500da79b187ac777
Abanoub Sameh <124413860+Abanoub-Sameh@users.noreply.github.com>
2026-06-06 03:20:19 +0300

    Add lecture 5

--- curriculum.js
+++ curriculum.js
@@ -51,6 +51,18 @@ const curriculumData = {
                 ],
                 "doctor_quiz": []
               }
+            },
+            {
+              "num": 5,
+              "title_ar": "33333",
+              "files": {
+                "original_ppt": [],
+                "translated_ppt": [],
+                "my_quiz": [
+                  "terms/term_1/anatomy/quizzes/Lecture_1_Fundamentals.html"
+                ],
+                "doctor_quiz": []
+              }
             }
           ]
         },
