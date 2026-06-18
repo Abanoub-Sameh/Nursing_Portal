c2904bf445ad859adec23eda95fafdbd415e3ec7
Abanoub Sameh <124413860+Abanoub-Sameh@users.noreply.github.com>
2026-06-18 23:17:27 +0300

    Add lecture 1

--- curriculum.js
+++ curriculum.js
@@ -232,7 +232,20 @@ const curriculumData = {
         },
         "english": {
           "name": "English",
-          "lectures": []
+          "lectures": [
+            {
+              "num": 1,
+              "title_ar": "بنك اسئله الميد ترم",
+              "files": {
+                "original_ppt": [
+                  "terms/term_1/english/original/Question Bank (1).pdf"
+                ],
+                "translated_ppt": [],
+                "my_quiz": [],
+                "doctor_quiz": []
+              }
+            }
+          ]
         },
         "biostatistics": {
           "name": "Biostatistics",
