b72fae3039eb8c6187bc0a0da9f7534a49118110
Abanoub Sameh <124413860+Abanoub-Sameh@users.noreply.github.com>
2026-06-27 10:06:42 +0300

    Update lecture 1

--- curriculum.js
+++ curriculum.js
@@ -299,10 +299,12 @@ const curriculumData = {
               "num": 1,
               "title_ar": "بنك اسئله الميد ترم",
               "files": {
-                "original_ppt": "terms/term_1/english/original/Question Bank (1).pdf",
-                "translated_ppt": "terms/term_1/english/guides/Quiz1.pdf",
-                "my_quiz": "",
-                "doctor_quiz": ""
+                "original_ppt": [
+                  "terms/term_1/english/original/Question Bank (1).pdf"
+                ],
+                "translated_ppt": [],
+                "my_quiz": [],
+                "doctor_quiz": []
               }
             }
           ]
